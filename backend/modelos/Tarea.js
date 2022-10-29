976fd0bdc4cc5ee1ab24cf7d948e581c4867eb50
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2022-10-29 15:21:53 -0500

    modelo de tarea added

---
+++ backend/modelos/Tarea.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const tareaSchema = mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    fechaEntrega: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
+    estado: {
+      type: Boolean,
+      default: false,
+    },
+    prioridad: {
+      type: String,
+      required: true,
+      enum: ["Baja", "Media", "Alta"],
+    },
+    proyecto: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Proyecto",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Tarea = mongoose.model("Tarea", tareaSchema);
+
+export default Tarea;
