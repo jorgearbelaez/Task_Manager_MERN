79811c0c3331907279e749624a2dfa7a83a18443
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2022-10-29 21:30:18 -0500

    creacion, obtencion, edicion y eliminacion de tareas

---
+++ backend/controllers/tareaController.js
@@ -0,0 +1,105 @@
+import Tarea from "../modelos/Tarea.js";
+import Proyecto from "../modelos/Proyecto.js";
+
+const agregarTarea = async (req, res) => {
+  const { proyecto } = req.body;
+
+  const existeProyecto = await Proyecto.findById(proyecto);
+
+  if (!existeProyecto) {
+    const error = new Error("Ese proyecto no existe en base de datos");
+    return res.status(404).json({ msg: error.message });
+  }
+
+  if (existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para añadir tareas");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    const tareaAlmacenada = await Tarea.create(req.body);
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const obtenerTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("No existe la tarea");
+    return res.status(404).json({ msg: error.message });
+  }
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para acceder a esta tarea");
+    return res.status(403).json({ msg: error.message });
+  }
+  try {
+    res.json(tarea);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const actualizarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("No existe la tarea");
+    return res.status(404).json({ msg: error.message });
+  }
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para acceder a esta tarea");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  tarea.nombre = req.body.nombre || tarea.nombre;
+  tarea.descripcion = req.body.descripcion || tarea.descripcion;
+  tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega;
+  tarea.prioridad = req.body.prioridad || tarea.prioridad;
+
+  try {
+    const tareaActualizada = await tarea.save();
+    res.json(tareaActualizada);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const eliminarTarea = async (req, res) => {
+  const { id } = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if (!tarea) {
+    const error = new Error("No existe la tarea");
+    return res.status(404).json({ msg: error.message });
+  }
+  if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para acceder a esta tarea");
+    return res.status(403).json({ msg: error.message });
+  }
+
+  try {
+    await Tarea.deleteOne();
+
+    res.json("Tarea eliminada");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const cambiarEstado = (req, res) => {};
+
+export {
+  agregarTarea,
+  obtenerTarea,
+  actualizarTarea,
+  eliminarTarea,
+  cambiarEstado,
+};
