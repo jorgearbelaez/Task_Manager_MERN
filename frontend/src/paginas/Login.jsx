5c1959a245585ac5d4f37462d32ff73f81ec0dff
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2022-11-01 12:06:47 -0500

    validando la autenticacion del usuario y almacenando el token en localSgtorage

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,11 +1,53 @@
-import { Link } from "react-router-dom"
+import {useState} from "react"
+import { Link, useNavigate } from "react-router-dom"
+import Alerta from "../components/Alerta"
+import clienteAxios from "../config/clienteAxios"
 
 const Login = () => {
+
+    const [email, setEmail]= useState('')
+    const [password, setPassword]= useState('')
+    const [alerta, setAlerta]= useState({})
+
+const handleSubmit = async e =>{
+    e.preventDefault()
+
+    if([email, password].includes('')){
+        setAlerta({
+        msg: 'Todos los campos son obligatorios',
+        error: true
+        })
+    return
+
+    }
+    
+    try {
+       const { data }= await clienteAxios.post('/usuarios/login', {
+            email,
+            password
+        })
+        setAlerta({})
+
+        localStorage.setItem('token', data.token)
+    } catch (error) {
+        setAlerta({
+            msg: error.response.data.msg,
+            error: true
+        })
+    }
+}
+const { msg } = alerta
   return (
     <>
-        <h1 className="text-white font-black text-6xl capitalize">Inicia sesión y administra tus {''}<span className="text-yellow-300">Proyectos</span></h1>
+        <h1 className="text-white font-black text-6xl capitalize">Inicia sesión y administra tus {''}<span className="text-yellow-300">Proyectos</span>
+        </h1>
 
-        <form  className="my-10 bg-white shadow rounded-lg p-10">
+        {msg && <Alerta alerta={alerta} />}
+
+        <form  
+            className="my-10 bg-white shadow rounded-lg p-10"
+            onSubmit={handleSubmit}
+        >
 
             <div className="my-5">
                 <label className="uppercase text-gray-900 block  text-xl font-bold"
@@ -15,7 +57,9 @@ const Login = () => {
                     id="email"
                     type="email"
                     placeholder="Ingresa tu Email"
-                    className="w-full mt-3 p-3 border rounded-xl bg-gray-200" />
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-200"
+                    value={email}
+                    onChange= {e=>setEmail(e.target.value)} />
 
             </div>
             <div className="my-5">
@@ -26,7 +70,9 @@ const Login = () => {
                     id="password"
                     type="password"
                     placeholder="Ingresa tu Contraseña"
-                    className="w-full mt-3 p-3 border rounded-xl bg-gray-200" />
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-200"
+                    value={password}
+                    onChange= {e=>setPassword(e.target.value)} />
 
             </div>
             <input 
