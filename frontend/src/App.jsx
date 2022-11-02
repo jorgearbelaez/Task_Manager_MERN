86f2b456631621f7dc1abf724d9d19b83424d950
Jorge Arbelaez Carmona <jorge.a.arbelaez.c@gmail.com>
2022-11-01 21:33:15 -0500

    creacion de context y hook de proyectos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Proyectos from './paginas/Proyectos'
 import NuevoProyecto from './paginas/NuevoProyecto'
 
 import {AuthProvider} from './context/AuthProvider'
+import {ProyectosProvider} from './context/ProyectosProvider'
 
 
 function App() {
@@ -20,21 +21,23 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-              <Route index element={<Login />} />
-              <Route path= "registrar" element={<Registrar />} />
-              <Route path= "olvide-password" element={<OlvidePassword />} />
-              <Route path= "olvide-password/:token" element={<NuevoPassword />} />
-              <Route path= "confirmar/:id" element={<ConfirmarCuenta />} />
-          </Route>
-          <Route path="/proyectos" element={<RutaProtegida />}>
-              <Route index element={<Proyectos />} />
-              <Route path='crear-proyecto' element={<nuevoProyecto />} />
-              
-          </Route>
+        <ProyectosProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+                <Route index element={<Login />} />
+                <Route path= "registrar" element={<Registrar />} />
+                <Route path= "olvide-password" element={<OlvidePassword />} />
+                <Route path= "olvide-password/:token" element={<NuevoPassword />} />
+                <Route path= "confirmar/:id" element={<ConfirmarCuenta />} />
+            </Route>
+            <Route path="/proyectos" element={<RutaProtegida />}>
+                <Route index element={<Proyectos />} />
+                <Route path='crear-proyecto' element={<nuevoProyecto />} />
+                
+            </Route>
 
-        </Routes>
+          </Routes>
+        </ProyectosProvider>
       </AuthProvider>
     </BrowserRouter>
   )
--- frontend/src/paginas/Proyectos.jsx
+++ frontend/src/paginas/Proyectos.jsx
@@ -1,9 +1,17 @@
-
+import useProyectos from "../hooks/useProyectos"
 
 const Proyectos = () => {
+
+  const {proyectos} = useProyectos()
+
   return (
     <>
         <h1 className="text-4xl font-black">Proyectos</h1>
+
+        <div>
+
+            
+        </div>
     
     
     
